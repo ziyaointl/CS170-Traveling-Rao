0040a08666348999b2ed9b3972bdf065f4c4aca6
Ziyao Zhang <ziyaointl@gmail.com>
2019-11-24 21:51:08 -0800

    Also calculate shortest paths

--- preprocess.py
+++ preprocess.py
@@ -6,8 +6,7 @@ import utils
 def preprocess(num_of_locations, num_houses, list_locations, list_houses, starting_car_location, adjacency_matrix):
     """
     Preprocess adjacency matrix
-    b. Generate a mapping from locations to node index in a hashmap
-    c. Put adjacency matrix in networkx
+
     d. Run networkx all_pairs_shortest_path
     e. Run floyd-warshall to get all pair-wise shortest distances
     f. Complete the graph using pair-wise shortest distances
@@ -23,14 +22,14 @@ def preprocess(num_of_locations, num_houses, list_locations, list_houses, starti
     adjacency_matrix = [[int(w*SCALE) if w != 'x' else w for w in row] for row in adjacency_matrix]
     print(adjacency_matrix)
 
-    #part b
+    # b. Generate a mapping from locations to node index in a hashmap
     map_locations = {}
     index = 0
     for location in list_locations:
         map_locations[location] = index
         index += 1
 
-    #part c
+    # c. Put adjacency matrix in networkx
     G = nx.Graph()
     for i in range(num_of_locations):
         G.add_node(i)
@@ -44,16 +43,20 @@ def preprocess(num_of_locations, num_houses, list_locations, list_houses, starti
                 if weight != 'x':
                     G.add_edge(j, k)
                     G.edges[j, k]['weight'] = weight
+                    print(G[j][k])
 
-    #part d, e
-    length = dict(nx.all_pairs_dijkstra_path_length(G, weight='weight'))
+    # d. Run networkx all_pairs_shortest_path
+    shortest_paths = dict(nx.all_pairs_dijkstra_path(G))
 
-    #part f
+    # e. Run floyd-warshall to get all pair-wise shortest distances 
+    shortest_paths_len = nx.floyd_warshall(G)
+
+    # f. Complete the graph using pair-wise shortest distances
     for m in range(num_of_locations):
         for n in range(m, num_of_locations):
             if not G.has_edge(m, n):
                 G.add_edge(m, n)
-            G.edges[m, n]['weight'] = length[m][n]
+            G.edges[m, n]['weight'] = shortest_paths_len[m][n]
 
     #part g
     product_prices = {}
