a6b8d62c610baac0a0957e620aca110cea2c63fb
Long He <longh@berkeley.edu>
2019-11-24 17:42:33 -0800

    finished preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,7 @@
-import student_utils.py
 import networkx as nx
 import numpy as np
+from student_utils import *
+import utils
 
 def preprocess(num_of_locations, num_houses, list_locations, list_houses, starting_car_location, adjacency_matrix):
     """
@@ -14,7 +15,7 @@ def preprocess(num_of_locations, num_houses, list_locations, list_houses, starti
     g. Generate product prices using pair-wise shortest distances
     h. Update all edges in G to 2/3
 
-    return the mapping from locations to index, the product prices(dict({location_index, dict({house_index, price})})), 
+    return the mapping from locations to index, the product prices(dict({location_index, dict({house_index, price})})),
     and the networkx preprocessed graph
     """
 
@@ -22,7 +23,7 @@ def preprocess(num_of_locations, num_houses, list_locations, list_houses, starti
     map_locations = {}
     index = 0
     for location in list_locations:
-        map_locations.update({location: index})
+        map_locations[location] = index
         index += 1
 
     #part b
@@ -30,11 +31,15 @@ def preprocess(num_of_locations, num_houses, list_locations, list_houses, starti
     for i in range(num_of_locations):
         G.add_node(i)
     for j in range(len(adjacency_matrix)):
-        for k in range(len(adjacency_matrix[0])):
-            weight = adjacency_matrix[j][k]
-            if weight != 'x':
+        for k in range(j, len(adjacency_matrix[0])):
+            if (j == k):
                 G.add_edge(j, k)
-                G.edges[j, k]['weight'] = weight
+                G.edges[j, k]['weight'] = 0
+            else:
+                weight = adjacency_matrix[j][k]
+                if weight != 'x':
+                    G.add_edge(j, k)
+                    G.edges[j, k]['weight'] = weight
 
     #part c not necessary
     #reachable = list(nx.dfs_preorder_nodes(G, source = map_locations[starting_car_location]))
@@ -43,25 +48,27 @@ def preprocess(num_of_locations, num_houses, list_locations, list_houses, starti
     #        G.remove_node(i)
 
     #part d, e
-    length = dict(nx.all_pairs_dijkstra_path_length(G))
+    length = dict(nx.all_pairs_dijkstra_path_length(G, weight='weight'))
 
     #part f
-    for m in range(num_locations):
-        for n in range(m + 1, num_locations):
+    for m in range(num_of_locations):
+        for n in range(m, num_of_locations):
+            if not G.has_edge(m, n):
+                G.add_edge(m, n)
             G.edges[m, n]['weight'] = length[m][n]
 
     #part g
     product_prices = {}
-    for m in range(num_locations):
+    for m in range(num_of_locations):
         local_prices = {}
         for n in range(num_houses):
             index_house = map_locations[list_houses[n]]
-            local_prices.update({index_house: G.edges[m, index_house]['weight']})
-        product_prices.update(m, local_prices)
+            local_prices[index_house] = G.edges[m, index_house]['weight']
+        product_prices[m] = local_prices
 
     #part h
-    for m in range(num_locations):
-        for n in range(m + 1, num_locations):
+    for m in range(num_of_locations):
+        for n in range(m + 1, num_of_locations):
             G.edges[m, n]['weight'] *= 2/3
 
     return map_locations, product_prices, G
