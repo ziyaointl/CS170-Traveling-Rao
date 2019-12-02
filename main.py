97d6dc04755c5b63993020f5db45aafef74a7088
Rui Chen <1986438490@qq.com>
2019-12-01 23:10:50 -0800

    Google TSP Solver Implemented

--- main.py
+++ main.py
@@ -13,6 +13,87 @@ from concorde.tsp import TSPSolver
 from collections import defaultdict
 from preprocess import preprocess
 
+from __future__ import print_function
+from ortools.constraint_solver import routing_enums_pb2
+from ortools.constraint_solver import pywrapcp
+
+
+
+def create_data_model(G,start,nodes):
+    """Stores the data for the problem."""
+    data = {}
+    data['distance_matrix'] = nx.to_numpy_matrix(G).tolist()
+    print(data['distance_matrix'])  # yapf: disable
+    data['num_vehicles'] = 1
+    data['depot'] = start
+    return data
+
+
+def print_solution(manager, routing, assignment):
+    """Prints assignment on console."""
+    #print('Objective: {} miles'.format(assignment.ObjectiveValue()))
+    index = routing.Start(0)
+    plan_output = []
+    route_distance = 0
+    while not routing.IsEnd(index):
+        plan_output.append(manager.IndexToNode(index))
+        previous_index = index
+        index = assignment.Value(routing.NextVar(index))
+        route_distance += routing.GetArcCostForVehicle(previous_index, index, 0)
+    #plan_output += ' {}\n'.format(manager.IndexToNode(index))
+    #print(plan_output)
+    #plan_output += 'Route distance: {}miles\n'.format(route_distance)
+    return plan_output
+
+
+def Google_inefficient_TSP(G,nodes,start,name):
+    """Entry point of the program."""
+    # Instantiate the data problem.
+    G = nx.subgraph(G, nodes)
+    data = create_data_model(G,start,nodes)
+
+    # Create the routing index manager.
+    manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']),
+                                           data['num_vehicles'], data['depot'])
+
+    # Create Routing Model.
+    routing = pywrapcp.RoutingModel(manager)
+
+
+    def distance_callback(from_index, to_index):
+        """Returns the distance between the two nodes."""
+        # Convert from routing variable Index to distance matrix NodeIndex.
+        from_node = manager.IndexToNode(from_index)
+        to_node = manager.IndexToNode(to_index)
+        return data['distance_matrix'][from_node][to_node]
+
+    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+
+    # Define cost of each arc.
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+    # Setting first solution heuristic.
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.first_solution_strategy = (
+        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+
+    # Solve the problem.
+    assignment = routing.SolveWithParameters(search_parameters)
+
+    # Print solution on console.
+    if assignment:
+        a = print_solution(manager, routing, assignment)
+        a = a + [a[0]]
+        print(a)
+    return a
+
+
+
+
+"""if __name__ == '__main__':
+    Google_inefficient_TSP()"""
+
+
 def superprint(*arg):
     print('------------------------------')
     print(*arg)
@@ -32,6 +113,38 @@ def TSP(G, nodes, start, name):
     cycle: solution
     """
     superprint('Calling TSP on', nodes, start)
+    def cal_TSP_cost(G, start, cycle):
+        l = len(cycle)
+        assert l <= 5, "should never call naive on too complex cases!"
+        travel_cost = 0
+        for i in range(1, l):
+            travel_cost += G[path[i - 1]][path[i]]['weight']
+        return travel_cost
+
+    def naive_TSP(G, nodes, start, name):
+        l = len(nodes)
+        nodes_cp = nodes[:]
+        nodes_cp.remove(start)
+        if l == 2:
+            return [start] + nodes_cp + [start]
+        elif l == 3:
+            sol1 = [start] + nodes_cp + [start]
+            sol2 = sol1[:]
+            sol2.reverse()
+            if cal_TSP_cost(G, start, sol1) > cal_TSP_cost(G, start, sol2):
+                return sol2
+            else:
+                return sol1
+        elif l == 4:
+            return Google_inefficient_TSP(G,nodes,start,name)
+        else:
+            print("incorrect parameter for naive TSP")
+
+    if len(nodes) <= 4 :
+        print("Calling naive TSP (made on brute force by Rui Chen)")
+        if len(nodes) < 2:
+            print("Too Simple testcase even for Naive TSP !")
+        return naive_TSP(G, nodes, start, name)
     def gen_output(G, filename):
         """Input:
         G: graph
